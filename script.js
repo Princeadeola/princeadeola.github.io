9f3025401735ba01103ee0bacdca2ec2308161a6
A-Adeola <56828114+Princeadeola@users.noreply.github.com>
2021-09-29 05:02:45 -0700

    Added new files

---
+++ script.js
@@ -0,0 +1,101 @@
+const navMenu = document.getElementById('nav_menu');
+const toggleMenu = document.getElementById('nav_toggle');
+const closeMenu = document.getElementById('nav_close');
+
+// Show
+toggleMenu.addEventListener('click', ()=>{
+	navMenu.classList.toggle('show')
+})
+
+// Hidden 
+closeMenu.addEventListener('click', ()=>{
+	navMenu.classList.remove('show')
+})
+
+// Remove Menu
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+	navMenu.classList.remove('show')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+//scroll section active link
+const sections = document.querySelectorAll('section[id]')
+
+window.addEventListener('scroll', scrollActive)
+
+function scrollActive(){
+	const scrollY = window.pageYoffset
+
+	sections.forEach(current => {
+		const sectionHeight = current.offsetHeight
+		const sectionTop = current.offsetTop - 50
+		sectionId = current.getAttribute('id')
+
+		if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+			document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.add('active')
+		}else {
+			document.querySelector('.nav_menu a[href*=' + sectionId + ']').classList.remove('active')
+		}
+	})
+}
+
+
+// Typing animation section
+var TxtType = function(el, toRotate, period) {
+        this.toRotate = toRotate;
+        this.el = el;
+        this.loopNum = 0;
+        this.period = parseInt(period, 10) || 2000;
+        this.txt = '';
+        this.tick();
+        this.isDeleting = false;
+    };
+
+    TxtType.prototype.tick = function() {
+        var i = this.loopNum % this.toRotate.length;
+        var fullTxt = this.toRotate[i];
+
+        if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+        } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+        }
+
+        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+        var that = this;
+        var delta = 200 - Math.random() * 100;
+
+        if (this.isDeleting) { delta /= 7; }
+
+        if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+        } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+        }
+
+        setTimeout(function() {
+        that.tick();
+        }, delta);
+    };
+
+    window.onload = function() {
+        var elements = document.getElementsByClassName('typewrite');
+        for (var i=0; i<elements.length; i++) {
+            var toRotate = elements[i].getAttribute('data-type');
+            var period = elements[i].getAttribute('data-period');
+            if (toRotate) {
+              new TxtType(elements[i], JSON.parse(toRotate), period);
+            }
+        }
+        // INJECT CSS
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
+    };
---
+++ submitform.js
@@ -0,0 +1,20 @@
+$(document).ready ->
+  $('#contact-form').submit (e) ->
+    name = document.getElementById('inputName')
+    email = document.getElementById('inputEmail')
+    message = document.getElementById('inputMessage')
+    if (!name.value or !email.value or !message.value){
+        alertify.error 'Please check your entries'
+        return false
+    }
+    else
+      {
+        $.ajax
+        method: 'POST'
+        url: '//formspree.io/techquile@gmail.com'
+        data: $('#contact-form').serialize()
+        datatype: 'json'
+        e.preventDefault()
+        $(this).get(0).reset()
+        alertify.success 'Message sent'
+      }
